ef8b1c3d59820f4f63592b498ad385114ddc7e55
arlen <1291187465@qq.com>
2023-08-29 19:24:12 +0800

    log

--- vendor/vendor/src/views/ShopSet/exchangeRates.vue
+++ vendor/vendor/src/views/ShopSet/exchangeRates.vue
@@ -17,8 +17,10 @@
             <el-row :gutter="20">
               <el-col :span="24">
                   <el-form ref="form" :model="form" label-width="80px">
-                    <el-form-item :label="$t('汇率值')">
-                      <el-input-number v-model="form.Rate" :placeholder="$t('shop.请输入有效数值')" :min="0" :precision="2" style="width:200px"></el-input-number>
+                    <el-form-item >
+                      {{$t('shop.汇率值')}} <el-radio v-model="radio" label="1"><el-input-number v-model="form.Rate" :placeholder="$t('shop.请输入有效数值')" :min="0" :precision="2" style="width:200px"></el-input-number></el-radio>
+                      {{$t('shop.利率')}} <el-radio v-model="radio" label="2"><el-input-number v-model="form.ratio" :placeholder="$t('shop.请输入有效数值')" :min="0" :precision="2" style="width:200px"></el-input-number></el-radio>
+                      
                     </el-form-item>
                     <el-form-item>
                       <el-button type="primary" @click="onSubmit">{{$t('shop.确定')}}</el-button>
@@ -39,7 +41,9 @@ export default {
     return {
         form:{
           Rate: '',
-        }
+          ratio: '',
+        },
+        radio: '1',
     };
   },
   mounted() {
@@ -48,11 +52,13 @@ export default {
   methods: {
     onSubmit() {
         this.$apiCall("api.User.setExchangeRate", {
-          exchangeRate: this.form.Rate,
+          exchangeRate: this.radio== 1 ?this.form.Rate : '',
+          productCoefficient: this.radio== 2? this.form.ratio : '',
         }, (r) => {
           if (r.ErrorCode == "9999") {
              this.$elementMessage(r.Message, "success");
              this.form.Rate = ''
+             this.form.ratio = ''
           } else {
             this.$elementMessage(r.Message, "error");
           }
